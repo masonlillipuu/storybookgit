a33c0f71c19cdfcef06e18d751362c8a86732dde
unknown <mason.lillipuu@gmail.com>
2021-05-23 12:03:11 -0800

    Initial comment

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -17,11 +17,5 @@ export const _Button = () => (
       <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{" "}
       <Button variant="light">Light</Button> <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
     </div>
-    <div>
-      <Button variant="outline-primary">Primary</Button> <Button variant="outline-secondary">Secondary</Button>{" "}
-      <Button variant="outline-success">Success</Button> <Button variant="outline-warning">Warning</Button>{" "}
-      <Button variant="outline-danger">Danger</Button> <Button variant="outline-info">Info</Button>{" "}
-      <Button variant="outline-light">Light</Button> <Button variant="outline-dark">Dark</Button>
-    </div>
   </>
 );
