5bca6b1d68b7a46179cfd8ba1b650cce641738c0
unknown <mason.lillipuu@gmail.com>
2021-05-23 12:07:03 -0800

    initial commit

---
+++ src/stories/alert.stories.js
@@ -0,0 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import React from "react";
+
+
+export default {
+  title: "Bootstrap",
+  component: Button,
+  parameters: { docs: { component: "Button" } },
+} ;
+
+export const _Button = () => (
+  <>
+    <div>
+      <Button variant="primary">Primary</Button> <Button variant="secondary">Secondary</Button>{" "}
+      <Button variant="success">Success</Button> <Button variant="warning">Warning</Button>{" "}
+      <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{" "}
+      <Button variant="light">Light</Button> <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
+    </div>
+  </>
+);
